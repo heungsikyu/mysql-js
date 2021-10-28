e8c86e8f232d3598a9712ab08abba2133029bfef
heungsikyu <hsyouatlas@gmail.com>
2021-10-29 00:13:09 +0900

    first commit

---
+++ ex_connect_select.js
@@ -0,0 +1,24 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'masterdetail'
+});
+
+connection.connect((err) => {
+  if(err){
+    console.error(`error connectiong: ${err.stack}`);
+    return;
+  }
+
+  console.log(`connected as id ${connection.threadId}`);
+
+});
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
